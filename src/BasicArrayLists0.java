3ff4667bd2398e701b7acf02ec1e04f91952ce36
Barb Baugher <barbbaugher@gmail.com>
2017-10-03 18:13:43 -0400

    Saving updates

---
+++ src/BasicArrayLists0.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class BasicArrayLists0 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> slots = new ArrayList<Integer>();
+		slots.add(-113);
+		slots.add(-113);
+		slots.add(-113);
+		slots.add(-113);
+		slots.add(-113);
+		slots.add(-113);
+		slots.add(-113);
+		slots.add(-113);
+		slots.add(-113);
+		slots.add(-113);
+		System.out.println(slots);
+
+	}
+
+}
